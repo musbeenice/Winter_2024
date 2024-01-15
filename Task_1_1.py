ed810adb99e4b8d84b197d46cdcf1995d6a724a5
musbeenice <qwertyuasfxxxafd8@gmail.com>
2024-01-15 23:35:27 +0300

    01.15 HomeTasks

---
+++ Task_1_1.py
@@ -0,0 +1,5 @@
+# Первая задача к первому занятию
+
+x = int(input())
+y = int(input())
+print(x + y, x * y)
---
+++ Task_1_2.py
@@ -0,0 +1,5 @@
+# Вторая задача к первому занятию
+
+x = int(input())
+y = int(input())
+print(max(x + y, x - y, x * y, x / y, x // y))
