ca799056eb700c1eba99a52e323347e0e976211e
musbeenice <qwertyuasfxxxafd8@gmail.com>
2024-01-31 23:19:13 +0300

    31.01

--- Task_8_1.py
+++ Task_8_1.py
@@ -1,1 +1,19 @@
 # Первая задача к восьмому занятию
+
+code = input()
+
+res = ""
+i = 0
+
+while i < len(code):
+    if i + 1 < len(code) and (code[i : i + 2] == "АГ" or code[i : i + 2] == "ГА"):
+        res += code[i + 1] + code[i]
+        i += 2
+    elif i + 1 < len(code) and (code[i : i + 2] == "ТЦ" or code[i : i + 2] == "ЦТ"):
+        res += "ТАГЦ" if code[i] == "Т" else "ЦАГТ"
+        i += 2
+    else:
+        res += code[i]
+        i += 1
+
+print(res)
--- Task_8_3.py
+++ Task_8_3.py
@@ -1,1 +1,10 @@
 # Третья задача к восьмому занятию
+
+lst = ["abab", "xx", "aaaaaaa", "abcbab"]
+
+
+def sort_lst(x):
+    return -len(set(x))
+
+
+print(sorted(lst, key=lambda x: (sort_lst(x), x)))
