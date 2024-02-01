1b93eb51c9ed75ffa3cefd4cc73fa4016620e8a7
musbeenice <qwertyuasfxxxafd8@gmail.com>
2024-02-01 23:12:56 +0300

    01.02

--- Task_8_2.py
+++ Task_8_2.py
@@ -1,4 +1,18 @@
 # Вторая задача к восьмому занятию
 
 lst = [[1, 55, 23], [27, 34, 11, 45], [3, 7, 9, 0, 78]]
+res = []
+def sorted_sublst():
+    for i in lst:
+        res.append(sorted(i, reverse = True))
+    return res
+print(sorted_sublst())
 
+def sorted_lst():
+    for i in lst:
+        sub_list = []
+        for j in ''.join(map(str, i)):
+            sub_list.append(int(j))
+    return len(set(sub_list))
+
+# до сортировки не дошел
