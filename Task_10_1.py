0e5098b28c697c00bb5d20a2042a4fc236a94490
musbeenice <qwertyuasfxxxafd8@gmail.com>
2024-02-05 22:08:49 +0300

    05.02

--- Task_10_1.py
+++ Task_10_1.py
@@ -1,1 +1,17 @@
 # Первая задача к десятому занятию
+
+s = ["Мой дядя самых честных правил\n", "Когда не в шутку занемог\n"]
+with open("test1.txt", "w", encoding="utf-8") as f:
+    f.writelines(s)
+
+with open("test1.txt", encoding="utf-8") as f_1:
+    s = f_1.readlines()
+    print(s)
+    with open("test2.txt", "w", encoding="utf-8") as f_2:
+        for i in s[::-1]:
+            w = " ".join(i.split()[::-1])
+            print(w, file=f_2)
+
+with open("test2.txt", encoding="utf-8") as f_3:
+    z = f_3.readlines()
+    print(z)
