59887ddb98c760e59b69b85245edb2baab1f4619
musbeenice <qwertyuasfxxxafd8@gmail.com>
2024-02-12 19:04:39 +0300

    12.02

--- Task_12_3.py
+++ Task_12_3.py
@@ -1,7 +1,20 @@
 # Третья задача к двеннадцатому занятию
 
-def func(str_of_ranges):
-    lst = [y for x in str_of_ranges for y in range(int(x.split("-")[0]), int(x.split("-")[1]))]
-    return lst
+# def func(str_of_ranges):
+#     lst = [y for x in str_of_ranges for y in range(int(x.split("-")[0]), int(x.split("-")[1]))]
+#     return lst
 
-print(func([x for x in input().split(", ")]))
+# print(func([x for x in input().split(",")]))
+
+# or
+
+s = input().split(",")
+res = []
+for z in s:
+    if "-" in z:
+        x, y = map(int, z.split("-"))
+        for u in range(x, y + 1):
+            res.append(u)
+    else:
+        res.append(int(z))
+print(res)
