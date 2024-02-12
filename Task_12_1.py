48281474fad14c1a75304db5cfc9c00e09c914b8
musbeenice <qwertyuasfxxxafd8@gmail.com>
2024-02-12 18:51:25 +0300

    12.02

--- Task_12_1.py
+++ Task_12_1.py
@@ -1,8 +1,8 @@
 # Первая задача к двеннадцатому занятию
 
 def func(lst):
-    lst_1 = [i for i in range(len(lst)) if lst[i] == min(lst)]
-    lst_2 = [i for i in range(len(lst)) if lst[i] == max(lst)]
+    lst_1 = [x for x in range(len(lst)) if lst[x] == min(lst)]
+    lst_2 = [x for x in range(len(lst)) if lst[x] == max(lst)]
     return lst_1, lst_2
 
 print(*func([int(x) for x in input().split()]))
--- Task_12_3.py
+++ Task_12_3.py
@@ -2,7 +2,6 @@
 
 def func(str_of_ranges):
     lst = [y for x in str_of_ranges for y in range(int(x.split("-")[0]), int(x.split("-")[1]))]
-    print([x for x in str_of_ranges])
     return lst
 
 print(func([x for x in input().split(", ")]))
