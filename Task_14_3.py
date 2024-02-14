187aca18e4a156cedf7525296e76d7b0bb736e83
musbeenice <qwertyuasfxxxafd8@gmail.com>
2024-02-14 21:43:15 +0300

    14.02

--- Task_14_3.py
+++ Task_14_3.py
@@ -1,2 +1,11 @@
 # Третья задача к четырнадцатому занятию
 
+def triangle(n):
+    if n == 1:
+        print("*")
+    else:
+        print("*" * n)
+        triangle(n - 1)
+    if n > 1:
+        print("*" * n)
+triangle(int(input()))
