0e2cb18901fd220e2b12920d97923da770fa5bd6
musbeenice <qwertyuasfxxxafd8@gmail.com>
2024-02-15 17:51:54 +0300

    15.02

--- Task_14_1.py
+++ Task_14_1.py
@@ -1,2 +1,9 @@
 # Первая задача к четырнадцатому занятию
 
+def num_dig(n, m = 0):
+    while n > 0:
+        m += 1
+        return num_dig(n // 10, m)
+    else:
+        return m
+print(num_dig(int(input())))
--- Task_14_2.py
+++ Task_14_2.py
@@ -1,2 +1,9 @@
 # Вторая задача к четырнадцатому занятию
 
+def total_dig(n, t = 0):
+    while n > 0:
+        dig = n % 10
+        t += dig
+        return total_dig(n // 10, t)
+    return t
+print(total_dig(int(input())))
