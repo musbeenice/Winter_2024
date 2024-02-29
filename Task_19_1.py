b3ecdbe986e7236830ff5dcfc8a4dacfcd2f50d2
musbeenice <qwertyuasfxxxafd8@gmail.com>
2024-02-29 22:56:28 +0300

    29.02

--- Task_19_1.py
+++ Task_19_1.py
@@ -4,7 +4,7 @@ from itertools import *
 
 res = []
 
-for seq in [[sum(x) for x in combinations([10, 50, 100, 200, 1000, 2000, 5000,], i)] for i in range(2, 8)]:
+for seq in [[sum(x) for x in combinations([10, 50, 100, 200, 1000, 2000, 5000,], i)] for i in range(1, 8)]:
     res.extend(seq)
 
 print(", ".join(map(str, sorted(res))))
