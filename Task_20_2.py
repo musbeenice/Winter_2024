6433545f0969df6db7b9be2e8a624e1b5c02aa6b
musbeenice <qwertyuasfxxxafd8@gmail.com>
2024-03-04 20:46:30 +0300

    04.03

--- Task_20_2.py
+++ Task_20_2.py
@@ -1,5 +1,26 @@
 # Вторая задача к двадцатому занятию
 
+# import pandas as pd
+#
+#
+# def total_num(df):
+#     total = 0
+#     for i in df.index:
+#         for j in df.columns:
+#             if isinstance(df.loc[i, j], (int, float)):
+#                 total += df.loc[i, j]
+#     print(format(total, ".2f"))
+#
+#
+# some_df = pd.DataFrame({
+#     "position": ["sofa", "wardrobe", "lamp"],
+#     "i_n": ["272490", "171251", "207600"],
+#     "price": [74.99, 34.99, 9.99]
+# })
+# total_num(some_df)
+
+# or
+
 import pandas as pd
 
 
@@ -7,8 +28,10 @@ def total_num(df):
     total = 0
     for i in df.index:
         for j in df.columns:
-            if isinstance(df.loc[i, j], (int, float)):
+            try:
                 total += df.loc[i, j]
+            except:
+                pass
     print(format(total, ".2f"))
 
 
--- Task_20_3.py
+++ Task_20_3.py
@@ -1,21 +1,62 @@
 # Третья задача к двадцатому занятию
 
-class InfSeq:
+# class InfSeq:
+#     def __init__(self):
+#         self.num = 0
+#
+#     def __iter__(self):
+#         return self
+#
+#     def __next__(self):
+#         if self.num > 25:
+#             self.num = self.num % 26
+#         self.num += 1
+#         self.let = chr(self.num + 64)
+#         return self.num, self.let
+#
+#
+# seq = InfSeq()
+# for _ in range(35):
+#     n, let = next(seq)
+#     print(f"{n}, {let},", end=" ")
+
+# or
+
+class NumLet:
     def __init__(self):
-        self.num = 0
+        self.index = 0
+        self.num_let = 1
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self.num > 25:
-            self.num = self.num % 26
-        self.num += 1
-        self.let = chr(self.num + 64)
-        return self.num, self.let
+        self.num_let = 1 - self.num_let
+        if self.num_let == 0:
+            self.index += 1
+            if self.index > 26: self.index = 1
+            return self.index
+        else:
+            return chr((ord("A")) + self.index - 1)
 
 
-seq = InfSeq()
-for _ in range(35):
-    n, let = next(seq)
-    print(f"{n}, {let},", end=" ")
+a = NumLet()
+for i in range(36):
+    print(next(a), end=" ")
+
+# or
+
+
+# def can():
+#     index = 0
+#     while True:
+#         if index >= 25:
+#             index = 0
+#         index += 1
+#         yield index
+#         yield chr(65 + index - 1)
+#
+#
+# f = can()
+# for _ in range(5):
+#     print(next(f), end=" ")
