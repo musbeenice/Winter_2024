ef606fcdbf3c0fd88635cbcabb6063e3fed367c1
musbeenice <qwertyuasfxxxafd8@gmail.com>
2024-03-16 22:05:53 +0300

    16.03

--- Task_25_2.py
+++ Task_25_2.py
@@ -2,17 +2,17 @@
 
 
 def is_palindrome(string):
-    def res_str(s):
+    def result_str(s):
         return [x for x in s.lower() if x.isalnum()]
 
     def are_chars_equal(s):
-        if len(s) == 1:
+        if len(s) <= 1:  # нужна именно такая проверка, из-за чётности/нечётности элементов
             return True
         if s[0] != s[-1]:
             return False
         return are_chars_equal(s[1:-1])
 
-    res_str = res_str(string)
+    res_str = result_str(string)
     return are_chars_equal(res_str)
 
 
