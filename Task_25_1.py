3463f58cb463b21f4c13722b167543f3890bd3e3
musbeenice <qwertyuasfxxxafd8@gmail.com>
2024-03-18 20:42:19 +0300

    18.03

--- Task_25_1.py
+++ Task_25_1.py
@@ -1,22 +1,43 @@
 # Первая задача к двадцать пятому занятию
 
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QVBoxLayout, QWidget
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("My Application")
-        self.count = 0
-        self.button = QPushButton("Press Me!")
-        # self.button.setCheckable(True)
+        self.setWindowTitle("C L I C K E R")
+        self.nazh = 0
+
+        self.button = QPushButton("CLICK HERE!")
+        self.button.setFixedSize(QSize(200, 200))
+        self.button.setCheckable(True)
         self.button.clicked.connect(self.the_button_was_clicked)
-        self.setCentralWidget(self.button)
+
+        self.label = QLabel("C L I C K E R")
+        font = self.label.font()
+        font.setPointSize(10)
+        self.label.setFont(font)
+        self.label.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter)
+
+        layout = QVBoxLayout()
+        widgets = [self.label, self.button]
+        for w in widgets:
+            layout.addWidget(w)
+
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+        self.tf = True
 
     def the_button_was_clicked(self):
-        self.count += 1
-        self.button.setText(f"{self.count}")
+        self.nazh += 1
+        print(f"Clicked {self.nazh} times!")
+        # self.label.setText((str(self.nazh)))
+        self.button.setText(str(self.nazh))
+        self.setWindowTitle(str(self.nazh))
 
 
 app = QApplication(sys.argv)
--- Task_25_2.py
+++ Task_25_2.py
@@ -1,20 +1,13 @@
 # Вторая задача к двадцать пятому занятию
 
 
-def is_palindrome(string):
-    def result_str(s):
-        return [x for x in s.lower() if x.isalnum()]
-
-    def are_chars_equal(s):
-        if len(s) <= 1:  # нужна именно такая проверка, из-за чётности/нечётности элементов
-            return True
-        if s[0] != s[-1]:
-            return False
-        return are_chars_equal(s[1:-1])
-
-    res_str = result_str(string)
-    return are_chars_equal(res_str)
+def is_palindrome(s):
+    if len(s) <= 1:  # нужна именно такая проверка, из-за чётности/нечётности элементов
+        return True
+    if s[0] == s[-1]:
+        return is_palindrome(s[1:-1])
+    else:
+        return False
 
 
-msg = input()
-print(is_palindrome(msg))
+print(is_palindrome(input()))
