9353721822dcde66fede96c1ced3ffdbd4aad165
musbeenice <qwertyuasfxxxafd8@gmail.com>
2024-03-25 21:37:46 +0300

    25.03

--- Task_28_1.py
+++ Task_28_1.py
@@ -1,15 +1,20 @@
 # Первая задача к двадцать восьмому занятию
 
-def num_of_inv(lst):
-    c = 0
-    for i in range(len(lst)):
-        for j in range(i + 1, len(lst)):
-            if lst[i] > lst[j]:
-                c += 1
-    return c
+lst = [5, 4, 3, 2, 1]
+print(sum([lst[i] > lst[j] for i in range(len(lst) - 1) for j in range(i + 1, len(lst))]))
 
+# or
 
-print(num_of_inv([1, 2, 3, 4, 5]))
-print(num_of_inv([5, 4, 3, 2, 1]))
-print(num_of_inv([1, 3, 2, 1, 0]))
-print(num_of_inv([1, 3, 2, 3, 1]))
+# def num_of_inv(lst):
+#     c = 0
+#     for i in range(len(lst)):
+#         for j in range(i + 1, len(lst)):
+#             if lst[i] > lst[j]:
+#                 c += 1
+#     return c
+#
+#
+# print(num_of_inv([1, 2, 3, 4, 5]))
+# print(num_of_inv([5, 4, 3, 2, 1]))
+# print(num_of_inv([1, 3, 2, 1, 0]))
+# print(num_of_inv([1, 3, 2, 3, 1]))
--- Task_28_3.py
+++ Task_28_3.py
@@ -1,16 +1,33 @@
 # Третья задача к двадцать восьмому занятию
 
-def hanoi(n, i=1, k=2):
+# def hanoi(n, i=1, k=2):
+#     count = 0
+#     if n == 1:
+#         print(f"Move disk 1 from pin {i} to {k}")
+#     else:
+#         tmp = 6 - i - k
+#         count += hanoi(n - 1, i, tmp)
+#         print(f"Move disk {n} from pin {i} to {k}")
+#         count += hanoi(n - 1, tmp, k)
+#     return count + 1
+#
+#
+# moves = hanoi(3)
+# print("Total moves:", moves)
+
+# or
+
+
+def hanoi(n, a, b):
     count = 0
-    if n == 1:
-        print(f"Move disk 1 from pin {i} to {k}")
+    if n == 0: return count
     else:
-        tmp = 6 - i - k
-        count += hanoi(n - 1, i, tmp)
-        print(f"Move disk {n} from pin {i} to {k}")
-        count += hanoi(n - 1, tmp, k)
-    return count + 1
+        count += hanoi(n - 1, a, 6 - a - b)
+        print(a, b)
+        count += 1
+        count += hanoi(n - 1, 6 - a - b, b)
+    return count
 
 
-moves = hanoi(3)
-print("Total moves:", moves)
+n = int(input())
+print(hanoi(n, 1, 2))
