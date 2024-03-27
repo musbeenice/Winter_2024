b5528deffd76d07362274a522d3503fa09f78994
musbeenice <qwertyuasfxxxafd8@gmail.com>
2024-03-27 19:00:42 +0300

    27.03

--- Task_29_1.py
+++ Task_29_1.py
@@ -1,10 +1,28 @@
 # Первая задача к двадцать девятому занятию
 
-def unique_num(lst):
-    dct = {}
-    for num in lst:
-        dct[num] = dct.get(num, 0) + 1
-    return filter(lambda x: dct[x] == 1, dct)
+from collections import Counter
 
 
-print(*unique_num([11, 3, 11, 11, 11, 11, 11, 11, 11, 11, 12, 12, 12, 12]))
+def one(lst):
+    c = Counter(lst)
+    for k, v in c.items():
+        if v == 1: return k
+
+
+print(one(map(int, input().split())))
+
+# or
+
+s = [11, 3, 11, 11, 11, 11, 11, 11, 11, 11, 12, 12, 12, 12]
+print(min(set(s), key=lambda x: s.count(x)))
+
+# or
+
+# def unique_num(lst):
+#     dct = {}
+#     for num in lst:
+#         dct[num] = dct.get(num, 0) + 1
+#     return filter(lambda x: dct[x] == 1, dct)
+#
+#
+# print(*unique_num([11, 3, 11, 11, 11, 11, 11, 11, 11, 11, 12, 12, 12, 12]))
--- Task_29_3.py
+++ Task_29_3.py
@@ -1,15 +1,13 @@
 # Третья задача к двадцать девятому занятию
 
 def are_isomorphic(w1, w2):
-    if len(w1) == len(w2):
-        dct1, dct2 = {}, {}
-        for i in range(len(w1)):
-            dct1[w1[i]] = dct1.get(w1[i], 0) + 1
-            dct2[w2[i]] = dct2.get(w2[i], 0) + 1
-        print(list(dct1.values()), list(dct2.values()))
-        return list(dct1.values()) == list(dct2.values())
-    else:
-        return False
+    if len(w1) != len(w2): return False
+    dct1, dct2 = {}, {}
+    for i in range(len(w1)):
+        dct1[w1[i]] = dct1.get(w1[i], 0) + 1
+        dct2[w2[i]] = dct2.get(w2[i], 0) + 1
+    print(list(dct1.values()), list(dct2.values()))
+    return list(dct1.values()) == list(dct2.values())
 
 
 # a = 'RAMBUNCTIOUSLY'
@@ -23,3 +21,24 @@ def are_isomorphic(w1, w2):
 a = 'ISOMORPHIC'
 b = 'GUNINJACGO'
 print(are_isomorphic(a, b))
+
+# or
+
+
+def isomorf(s, z):
+    d = {}
+    if len(s) != len(z):
+        return False, 1
+    for i in range(len(s)):
+        if s[i] in d:
+            if z[i] != d[s[i]]: return False, 2
+        else:
+            if z[i] in d.values(): return False, 3
+            d[s[i]] = z[i]
+        print(i, s[i], z[i], d)
+    return True
+
+
+a = input()
+b = input()
+print(isomorf(a, b))
